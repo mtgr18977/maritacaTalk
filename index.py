94b2d0c1e81713763d242c0ff2317d34cf4803c4
Paulo Pilotti Duarte <guilhermepilotti@gmail.com>
2024-10-14 14:59:32 -0300

    Maritaca-Chat
    Update
    - PY
    - Requirements

---
+++ index.py
@@ -0,0 +1,38 @@
+import openai
+import streamlit as st
+
+# Configurando a API da Maritaca
+client = openai.OpenAI(
+    api_key="108975062970243465496_28ca7f06ad1187a2",
+    base_url="https://chat.maritaca.ai/api",
+)
+
+# Configuração da interface do Streamlit
+st.title("Fale com a Maritaca")
+
+# Caixa de entrada de texto para a mensagem do usuário
+user_input = st.text_input("Digite sua mensagem:")
+
+# Botão de enviar
+if st.button("Enviar"):
+    if user_input:
+        # Criando a mensagem a ser enviada para a API
+        messages = [
+            {"role": "user", "content": user_input},
+        ]
+
+        # Chamando a API para gerar a resposta
+        response = client.chat.completions.create(
+            model="sabia-3",
+            messages=messages,
+            temperature=0.7,
+            max_tokens=512,
+        )
+
+        # Extraindo a resposta da API
+        answer = response.choices[0].message.content
+
+        # Exibindo a resposta formatada
+        st.markdown(f"**Resposta do chatbot:** {answer}")
+    else:
+        st.warning("Por favor, insira uma mensagem antes de clicar em Enviar.")
